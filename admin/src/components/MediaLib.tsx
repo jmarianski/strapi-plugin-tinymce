6f185acfaa8a9e8bb10f68c8063f645dffd912bd
Jacek Mariański <jmarianski@trans.eu>
2025-07-03 12:56:20 +0200

    remove local url for images from media library

--- admin/src/components/MediaLib.tsx
+++ admin/src/components/MediaLib.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { prefixFileUrlWithBackendUrl } from '../utils/prefixFileUrlWithBackendUrl';
 import { useStrapiApp } from '@strapi/strapi/admin';
 
 interface MediaLibProps {
@@ -12,11 +11,10 @@ const MediaLib = ({ isOpen, onChange, onToggle }: MediaLibProps) => {
     const mediaLibraryDialog = useStrapiApp('mediaLibrary', (state: any) => state.components);
     const MediaDialog = mediaLibraryDialog['media-library'];
 
-
     const handleSelectAssets = (files: any) => {
         const formattedFiles = files.map((f: any) => ({
             alt: f.alternativeText || f.name,
-            url: prefixFileUrlWithBackendUrl(f.url),
+            url: f.url,
             mime: f.mime,
         }));
 
